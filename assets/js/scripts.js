f0cc4c09f6fefad48cf5542350fb57f43ef42cc3
Travis Flatt <tflatt74@gmail.com>
2015-12-29 17:55:48 -0600

    Format HC data

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -70,6 +70,7 @@ new Vue({
 			var seasonStats = new StatsBlock();
 			var ypaChartData = [];
 			var cmpChartData = [];
+			var team = "";
 
 			for(var i = 0; i < this.playerStats.rows.length; i++) {
 				var row = this.playerStats.rows[i];
@@ -83,19 +84,32 @@ new Vue({
 					seasonStats.Rush += row[16];
 					seasonStats.RshYds += row[17];
 					seasonStats.RshTD += row[18];
-					ypaChartData.push([row[5], parseFloat((row[14]/row[10]).toFixed(1))]);
-					cmpChartData.push([row[5], parseFloat(((row[11]/row[10])*100).toFixed(1))]);
+					
+					if (parseFloat((row[14]/row[10]).toFixed(1)) >= 0) {
+						ypaChartData.push([row[5], parseFloat((row[14]/row[10]).toFixed(1))]);
+					} else {
+						ypaChartData.push([row[5], null]);
+					}
+
+					if(parseFloat(((row[11]/row[10])*100).toFixed(1)) >= 0) {
+						cmpChartData.push([row[5], parseFloat(((row[11]/row[10])*100).toFixed(1))]);
+					} else {
+						cmpChartData.push([row[5], null]);
+					}
+
+					team = row[6];
 				}
 			}
 			this.seasonStats = seasonStats;
 
-			this.buildCharts(ypaChartData, cmpChartData);
+			this.buildCharts(ypaChartData, cmpChartData, team);
 		},
 
 		getTotalStats: function() {
 			var totalStats = new StatsBlock();
 			var ypaChartData = [];
 			var cmpChartData = [];
+			var team = "";
 
 			for(var i = 0; i < this.playerStats.rows.length; i++) {
 				var row = this.playerStats.rows[i];
@@ -108,27 +122,126 @@ new Vue({
 				totalStats.Rush += row[16];
 				totalStats.RshYds += row[17];
 				totalStats.RshTD += row[18];
-				ypaChartData.push([row[5], parseFloat((row[14]/row[10]).toFixed(1))]);
-				cmpChartData.push([row[5], parseFloat(((row[11]/row[10])*100).toFixed(1))]);
+
+				var ypaData = {
+					x: Date.parse(row[5]),
+					y: null,
+					year: row[3],
+					week: row[4],
+					game: {
+						team: row[6],
+						teamImg: row[7],
+						opp: row[8],
+						oppImg: row[9]
+					},
+					stats: {
+						Att: row[10],
+						Cmp: row[11],
+						Sack: row[12],
+						Int: row[13],
+						PsYds: row[14],
+						PsTD: row[15],
+						Rush: row[16],
+						RshYds: row[17],
+						RshTD: row[18]
+					},
+					name: "Week " + row[4] + ", " + row[3]
+				};
+
+				if(parseFloat((row[14]/row[10]).toFixed(1)) >= 0 ) {
+					ypaData.y = parseFloat((row[14]/row[10]).toFixed(1));
+				}
+
+				ypaChartData.push(ypaData); 
+
+				var cmpData = {
+					x: Date.parse(row[5]),
+					y: null,
+					year: row[3],
+					week: row[4],
+					game: {
+						team: row[6],
+						teamImg: row[7],
+						opp: row[8],
+						oppImg: row[9]
+					},
+					stats: {
+						Att: row[10],
+						Cmp: row[11],
+						Sack: row[12],
+						Int: row[13],
+						PsYds: row[14],
+						PsTD: row[15],
+						Rush: row[16],
+						RshYds: row[17],
+						RshTD: row[18]
+					},
+					name: "Week " + row[4] + ", " + row[3]
+				};
+
+				if(parseFloat(((row[11]/row[10])*100).toFixed(1)) >= 0) {
+					cmpData.y = parseFloat(((row[11]/row[10])*100).toFixed(1));
+				}
+				cmpChartData.push(cmpData); 
+				team = row[6];
 			}
 
 			this.totalStats = totalStats;
-
 			// Build Charts
-			this.buildCharts(ypaChartData, cmpChartData);
+			this.buildCharts(ypaChartData, cmpChartData, team);
 		},
 
-		buildCharts: function(ypa, cmp) {
+		buildCharts: function(ypa, cmp, team) {
 			// Build Charts
+			var primary = '';
+			var secondary = '';
+
+			console.log(ypa);
+
+			switch(team) {
+				case 'CIN':
+					primary = "#FB4F14";
+					secondary = "#FFFFFF";
+					break;
+				case 'DAL':
+					primary = "#0D254C";
+					secondary = "#C5CED6";
+					break;
+				case 'NE':
+					primary = "#0D254C";
+					secondary = "#D6D6D6";
+					break;
+				default:
+					primary = "#7CB5EC";
+					secondary = "#EFEFEF";
+			}
+
 			$('#yards-per-attempt').highcharts({
 		        chart: {
-		            type: 'line',
+		            type: 'spline',
+		            plotBackgroundColor: {
+		            	linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
+					    stops: [
+					        [0, secondary],
+					        [1, primary],
+					    ]
+		            },
+		            plotBorderColor: secondary,
+		            plotBorderWidth: 2,
+		            borderColor: primary,
+		            borderWidth: 2,
+		            className: team,
+		            renderTo: 'yards-per-attempt'
+		        },
+		        credits: {
+		        	href: "https://github.com/travtex",
+		        	text: "Hire This Guy"
 		        },
 		        title: {
 		            text: 'Yards per Attempt'
 		        },
 		        xAxis: {
-		       
+		       		type: 'date'
 		        },
 		        yAxis: {
 		            title: {
@@ -137,19 +250,21 @@ new Vue({
 		        },
 		        series: [{
 		            name: 'YPA',
-		            data: ypa
+		            data: ypa,
+		            color: secondary,
+		            dashStyle: "ShortDashDotDot"
 		        }]
 		    });
 
 		    $('#completion-percentage').highcharts({
 		        chart: {
-		            type: 'line',
+		            type: 'spline',
 		        },
 		        title: {
 		            text: 'Completion Percentage'
 		        },
 		        xAxis: {
-		        
+		        	type: 'date'
 		        },
 		        yAxis: {
 		            title: {
